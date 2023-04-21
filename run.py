fb43fc3726e963cc8a993383f876bcbc6a7d19ed
Claudio Correa <clcorrea@udec.cl>
2023-04-21 14:48:21 -0400

    first commit

---
+++ run.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+
+def run_flask():
+    print('start APP in Flask..')
+    os.environ['FLASK_APP'] = 'app'
+    '''
+    os.environ['FLASK_DATABASE_HOST'] = 'localhost'
+    os.environ['FLASK_DATABASE_PASSWORD'] = 'comoqueso123'
+    os.environ['FLASK_DATABASE_USER'] = 'lukeroot'
+    os.environ['FLASK_DATABASE'] = 'prueba'
+    print('start database')
+    subprocess.call(["flask","init-db"])
+    
+    
+    '''
+    print('start APP as development..')
+    os.environ['FLASK_VPN'] = 'development'
+    print('flask run..')
+    subprocess.call(["flask","run"])
+    
+    #subprocess.call(["set"])
+    print('Done')
+run_flask()
